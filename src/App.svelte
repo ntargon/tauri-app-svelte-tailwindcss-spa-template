dcb3099bb9be00af80274dd282eb056612f6cd45
ntargon <argon19951128@gmail.com>
2022-11-05 09:44:07 +0900

    svelte-spa-routerを導入
    ```
    $ yarn add svelte-spa-router
    ```
    
    * サンプルページも作成

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import Greet from './lib/Greet.svelte'
   import "./app.css"
+  import Router from 'svelte-spa-router'
+  import { routes } from './router'
+  import { push } from 'svelte-spa-router';
 </script>
 
 <main class="container">
@@ -16,6 +19,13 @@
 
   <div class="bg-red-500">
     bg-red-500
+  </div>
+
+  <Router {routes} />
+
+  <div>
+    <button on:click={() => push('/')}>Page0</button>
+    <button on:click={() => push('/page1')}>Page1</button>
   </div>
 
 </main>
---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import Page0 from '../pages/Page0.svelte'
+import Page1 from '../pages/Page1.svelte'
+
+export const routes = {
+  '/': Page0,
+  '/page0': Page0,
+  '/page1': Page1,
+}

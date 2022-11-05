6b50bb7d5acea1f4be422b9b134db5fbc6c793c9
ntargon <argon19951128@gmail.com>
2022-11-05 14:48:29 +0900

    Shift+Spaceでshow&focus. Escapeでhide

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte';
   import Header from './lib/Header.svelte';
   import './app.css'
   import Router from 'svelte-spa-router';
   import { routes } from './router';
   import { register, unregisterAll } from '@tauri-apps/api/globalShortcut';
   import { onMount } from 'svelte';
+  import { appWindow } from '@tauri-apps/api/window';
 
   onMount(async () => {
     await unregisterAll();
-    await register('CommandOrControl+Shift+C', () => {
-      console.log('CommandOrControl+Shift+C');
+    await register('Shift+Space', async () => {
+      console.log('shortcut');
+      await appWindow.show();
+      await appWindow.setFocus();
     });
   })
+
+  /**
+   * Escapeキーでウィンドウを隠す
+   * @param e
+   */
+  const onKeyDown = async (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      e.preventDefault();
+      await appWindow.hide();
+    }
+  }
 </script>
 
 <main class="mx-5">
@@ -22,6 +35,8 @@
     <Router {routes} />
   </div>
 </main>
+
+<svelte:window on:keydown={onKeyDown} />
 
 <style>
 </style>
